16b774df45bd0f60c079ec96ab2175bd2b17713b
Josh Klein <kleinjjosh74@gmail.com>
2022-05-19 14:53:31 -0400

    updated references/added some validation to models

--- models/Product.js
+++ models/Product.js
@@ -12,6 +12,7 @@ Product.init(
     // define columns
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
@@ -21,15 +22,26 @@ Product.init(
     },
     price: {
       type: DataTypes.DECIMAL,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
